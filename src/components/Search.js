d0a67a1ecd7f0e7ffbfeb01666afc598d09cca10
joinlooks <joinlooks@gmail.com>
2022-12-19 20:18:16 +0530

    User can search by just pressing enter (form tag added)

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,37 +16,39 @@ const Search = (props) => {
 	return (
 		<Card marginTop={"5"}>
 			<CardBody>
-				<FormControl>
-					<FormLabel>Type here...</FormLabel>
-					<Input
-						type="text"
-						id="text"
-						placeholder="Search"
-						fontSize={"lg"}
-						onChange={(e) => {
-							setLine(e.target.value);
-						}}
-					/>
-					<Box
-						display={"flex"}
-						alignItems="center"
-						justifyContent={"center"}
-						marginTop="3"
-					>
-						<Button
-							type="submit"
-							colorScheme="blue"
-							size="sm"
-							width={"100%"}
-							onClick={(e) => {
-								e.preventDefault();
-								setText(line);
+				<form>
+					<FormControl>
+						<FormLabel>Type here...</FormLabel>
+						<Input
+							type="text"
+							id="text"
+							placeholder="Search"
+							fontSize={"lg"}
+							onChange={(e) => {
+								setLine(e.target.value);
 							}}
+						/>
+						<Box
+							display={"flex"}
+							alignItems="center"
+							justifyContent={"center"}
+							marginTop="3"
 						>
-							Search
-						</Button>
-					</Box>
-				</FormControl>
+							<Button
+								type="submit"
+								colorScheme="blue"
+								size="sm"
+								width={"100%"}
+								onClick={(e) => {
+									e.preventDefault();
+									setText(line);
+								}}
+							>
+								Search
+							</Button>
+						</Box>
+					</FormControl>
+				</form>
 			</CardBody>
 		</Card>
 	);
